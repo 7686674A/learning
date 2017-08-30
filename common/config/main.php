06743fc252271e0ce64b010ad8ea280209ab7af0
7686674A <2460855072@qq.com>
2017-08-30 16:08:08 +0800

    first save

---
+++ common/config/main.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'helper' => [
+            'class' => 'common\components\Helper',
+            'property' => '123',
+        ],
+    ],
+];
